a95a779b4979aa1ce0d698c8affda3d016e644c1
Niemi <70947133+nniemi@users.noreply.github.com>
2021-11-20 13:36:28 +0200

    Functions and commenting done, bit more optimizing

--- prg1/datastructures.cc
+++ prg1/datastructures.cc
@@ -10,6 +10,7 @@
 
 #include <cmath>
 #include <unordered_map>
+#include <map>
 
 std::minstd_rand rand_engine; // Reasonably quick pseudo-random generator
 
@@ -40,29 +41,44 @@ Datastructures::Datastructures()
 
 Datastructures::~Datastructures()
 {
-    // Write any cleanup you need here
+    // Calls for clear function
+    clear_all();
 }
 
 unsigned int Datastructures::town_count()
 {
 
+    // Returns the size of the database.
     return database.size();
 
 }
 
 void Datastructures::clear_all()
 {
+
+
+    // Deletes the pointers
+    for(auto& town_pointer : database) {
+        delete town_pointer.second;
+    }
+
+    // Clears the database
     database.clear();
 }
 
 bool Datastructures::add_town(TownID id, const Name &name, Coord coord, int tax)
 {
 
-    if(database.find(id) != database.end())
+    const auto town = database.find(id);
+
+    // Checks if the town exists.
+    if(town != database.end())
         return false;
 
+    // Factors the variables to the Town struct.
     Town* new_town = new Town;
     database.insert(std::make_pair(id, new_town));
+    new_town->id = id;
     new_town->name = name;
     new_town->coord = coord;
     new_town->tax = tax;
@@ -72,28 +88,44 @@ bool Datastructures::add_town(TownID id, const Name &name, Coord coord, int tax)
 
 Name Datastructures::get_town_name(TownID id)
 {
-    if (database.find(id) == database.end())
+
+    const auto town = database.find(id);
+
+    // Checks if the town exists.
+    if (town == database.end())
         return NO_NAME;
-    return database.at(id)->name;
+    return town->second->name;
 }
 
 Coord Datastructures::get_town_coordinates(TownID id)
 {
-    if (database.find(id) == database.end())
+
+
+    const auto town = database.find(id);
+
+    // Checks if the town exists.
+    if (town == database.end())
         return NO_COORD;
-    return database.at(id)->coord;
+    return town->second->coord;
 }
 
 int Datastructures::get_town_tax(TownID id)
 {
-    if (database.find(id) == database.end())
+    const auto town = database.find(id);
+    if (town == database.end())
         return NO_VALUE;
-    return database.at(id)->tax;
+    return town->second->tax;
 }
+
 
 std::vector<TownID> Datastructures::all_towns()
 {
-    std::vector<std::string> all;
+    std::vector<TownID> all = {};
+
+    // Reserving the vector
+    all.reserve(database.size());
+
+    // Loops through and stores the TownIDs to the vector
     for (auto& i : database) {
         all.push_back(i.first);
     }
@@ -102,132 +134,348 @@ std::vector<TownID> Datastructures::all_towns()
 
 std::vector<TownID> Datastructures::find_towns(const Name &name)
 {
+    // Creating a vector to store names
     std::vector<TownID> towns;
 
+    // Looping through the database and storing the names.
     for (auto& i : database) {
         if(i.second->name == name)
-            towns.push_back(name);
+            towns.push_back(i.first);
     }
     return towns;
 }
 
 bool Datastructures::change_town_name(TownID id, const Name &newname)
 {
+    const auto town = database.find(id);
 
     // Checking if the town exists.
-    if(database.find(id) == database.end())
+    if(town == database.end())
         return false;
 
     // Renaming the city.
-    database.at(id)->name = newname;
+    town->second->name = newname;
     return true;
 }
 
-std::vector<TownID> Datastructures::towns_alphabetically()
-{
-    // Creating a vector
-    std::vector<TownID> towns;
+std::vector<TownID> Datastructures::towns_alphabetically() {
 
-    // Reserving memoryspace for the vector.
+    // Creating a vector and a multimap
+    std::vector<Town*> towns;
+
+    // Reserving the vector
     towns.reserve(database.size());
 
-    // Going through the database and pushing TownIDs to the vector.
-    for(auto& i : database) {
-        towns.push_back(i.first);
+
+    for(auto& town : database) {
+        towns.push_back(town.second);
     }
 
-    // Sorting by names of the towns, using lambda function to
-    // find the correct sorting.
-    std::sort(towns.begin(), towns.end(),[this](TownID a, TownID b)
-        {return database.at(a)->name < database.at(b)->name;});
+    // Sorting the vector by town name.
+    std::sort(towns.begin(), towns.end(),[](Town* a, Town* b)
+            {return a->name < b->name;});
+
+
+    std::vector<TownID> towns_ordered;
+
+    for(auto& town : towns) {
+        towns_ordered.push_back(town->id);
+    }
+
 
     // Returning the vector.
-    return towns;
+    return towns_ordered;
+
+
 
 }
 
 std::vector<TownID> Datastructures::towns_distance_increasing()
 {
-    // Creating a vector
-    std::vector<TownID> towns;
 
-    // Reserving memoryspace for the vector.
-    towns.reserve(database.size());
+    // Creating a vector
+    std::vector<TownID> towns_ordered;
+    std::multimap<int,TownID> towns;
+
+    // Reserving the vector
+    towns_ordered.reserve(database.size());
+
+    // Constant for coordinates
+    const Coord origin = {0,0};
 
     // Going through the database and pushing TownIDs to the vector.
     for(auto& i : database) {
-        towns.push_back(i.first);
+        towns.insert({distance_from_point(origin,(i.second->coord)),i.first});
     }
 
     // Sorting by the distance to origin (0,0), using lambda function to
     // find the correct sorting.
-    std::sort(towns.begin(), towns.end(),[this](TownID a, TownID b)
-        {return sqrt(pow((0 - database.at(a)->coord.x),2.0) +
-                     pow((0 - database.at(a)->coord.y),2.0)) <
-                sqrt(pow((0 - database.at(b)->coord.x),2.0) +
-                     pow((0 - database.at(b)->coord.y),2.0));});
+
+    for (auto& i : towns) {
+        towns_ordered.push_back(i.second);
+    }
+
 
     // Returning the vector.
-    return towns;
+    return towns_ordered;
+
 }
 
 TownID Datastructures::min_distance()
 {
-    // Replace the line below with your implementation
-    throw NotImplemented("min_distance()");
+
+    // Factoring the origin
+    const Coord origin = {0,0};
+
+    // Sorts the database and finds the smallest element.
+    return  std::min_element(database.begin(),database.end(),[this,origin] (const auto& a, const auto& b)
+    {return distance_from_point(origin,(a.second->coord)) < distance_from_point(origin,(b.second->coord));})->first;
+
+
 }
 
 TownID Datastructures::max_distance()
 {
-    // Replace the line below with your implementation
-    throw NotImplemented("max_distance()");
+    // Factoring the origin
+    const Coord origin = {0,0};
+
+
+    // Sorts the database and finds the biggest element.
+    return  std::max_element(database.begin(),database.end(),[this,origin] (const auto& a, const auto& b)
+    {return distance_from_point(origin,(a.second->coord)) < distance_from_point(origin,(b.second->coord));})->first;
 }
 
-bool Datastructures::add_vassalship(TownID /*vassalid*/, TownID /*masterid*/)
+bool Datastructures::add_vassalship(TownID vassalid, TownID masterid)
 {
-    // Replace the line below with your implementation
-    // Also uncomment parameters ( /* param */ -> param )
-    throw NotImplemented("add_vassalship()");
+
+
+    const auto vassal = database.find(vassalid);
+    const auto master = database.find(masterid);
+
+    // Checks if either IDs are missing and if the vassal doesn't have a parent town.
+    if (vassal == database.end() || master == database.end() || vassal->second->parent != nullptr) {
+        return false;
+    }
+
+    // Sets the vassalships
+    vassal->second->parent = master->second;
+    master->second->neighbour.push_back(vassal->second);
+
+    return true;
 }
 
-std::vector<TownID> Datastructures::get_town_vassals(TownID /*id*/)
+std::vector<TownID> Datastructures::get_town_vassals(TownID id)
 {
-    // Replace the line below with your implementation
-    // Also uncomment parameters ( /* param */ -> param )
-    throw NotImplemented("get_town_vassals()");
+
+
+    std::vector<TownID> towns;
+    const auto town = database.find(id);
+
+    // Reserving the vector.
+    towns.reserve(town->second->neighbour.size());
+
+    // Checks if the town exists.
+    if(town == database.end()) {
+        towns.push_back(NO_TOWNID);
+        return towns;
+    }
+
+    // Checks if the town doesn't have any neighbours.
+    if (town->second->neighbour.empty()) {
+
+        return towns;
+    }
+
+    // Pushbacks if a town was found.
+    for (auto& i : town->second->neighbour) {
+        towns.push_back(i->id);
+    }
+    return towns;
+
 }
 
-std::vector<TownID> Datastructures::taxer_path(TownID /*id*/)
+std::vector<TownID> Datastructures::taxer_path(TownID id)
 {
-    // Replace the line below with your implementation
-    // Also uncomment parameters ( /* param */ -> param )
-    throw NotImplemented("taxer_path()");
+
+    std::vector<TownID> towns = {};
+    const auto town = database.find(id);
+
+    if(town == database.end()) {
+        towns.push_back(NO_TOWNID);
+        return towns;
+    }
+
+    // Adds the city to the vector.
+    towns.push_back(id);
+
+    // Loops through the parents and stores them to the vector;
+    while(town->second->parent != nullptr) {
+        towns.push_back(town->second->parent->id);
+        id = town->second->parent->id;
+    }
+
+    return towns;
+
 }
 
-bool Datastructures::remove_town(TownID /*id*/)
+bool Datastructures::remove_town(TownID id)
 {
-    // Replace the line below with your implementation
-    // Also uncomment parameters ( /* param */ -> param )
-    throw NotImplemented("remove_town()");
+
+
+    const auto town = database.find(id);
+
+    // Checking if the id exists in the database.
+    if(town == database.end())
+        return false;
+
+
+    // Checks if the id has a parent, if so, adds the vassals to the parents
+    // neighbour. If the id doesn't have a parent, clears the neighbour vector
+    if (town->second->parent != nullptr) {
+        for (auto& i : town->second->neighbour) {
+            town->second->parent->neighbour.push_back(i);
+        }
+    } else {
+        for(auto& i : town->second->neighbour) {
+            delete i;
+
+        }
+        town->second->neighbour.clear();
+    }
+
+    // Erases the element from the database
+    database.erase(database.find(id));
+    return true;
 }
 
-std::vector<TownID> Datastructures::towns_nearest(Coord /*coord*/)
+std::vector<TownID> Datastructures::towns_nearest(Coord coord)
 {
-    // Replace the line below with your implementation
-    // Also uncomment parameters ( /* param */ -> param )
-    throw NotImplemented("towns_nearest()");
+
+    // Creating a vector
+    std::vector<TownID> towns_ordered;
+    std::multimap<int,TownID> towns;
+
+    towns_ordered.reserve(database.size());
+
+    // Going through the database and pushing TownIDs to the vector.
+    for(auto& i : database) {
+        towns.insert({distance_from_point(coord, i.second->coord),i.first});
+    }
+
+    // Sorting by the distance to origin (0,0), using lambda function to
+    // find the correct sorting.
+    for (auto& i : towns) {
+        towns_ordered.push_back(i.second);
+    }
+
+    // Returning the vector.
+    return towns_ordered;
+
 }
 
-std::vector<TownID> Datastructures::longest_vassal_path(TownID /*id*/)
+std::vector<TownID> Datastructures::longest_vassal_path(TownID id)
 {
-    // Replace the line below with your implementation
-    // Also uncomment parameters ( /* param */ -> param )
-    throw NotImplemented("longest_vassal_path()");
+
+    std::vector<TownID> longest_towns;
+    std::vector<TownID> current_towns;
+
+    const auto town = database.find(id);
+
+    // Checks if the town exists in the database
+    if(town == database.end()) {
+        longest_towns.push_back(NO_TOWNID);
+        return longest_towns;
+    }
+
+    // Checks the towns neighbours.
+    if (town->second->neighbour.empty()) {
+        longest_towns.push_back(town->second->id);
+        return longest_towns;
+    }
+
+    // Starts the recursive fuction.
+    recursive_longest_path(town->second,current_towns, longest_towns);
+
+    return longest_towns;
+
+
+
 }
 
-int Datastructures::total_net_tax(TownID /*id*/)
+int Datastructures::total_net_tax(TownID id)
 {
-    // Replace the line below with your implementation
-    // Also uncomment parameters ( /* param */ -> param )
-    throw NotImplemented("total_net_tax()");
+    const auto town = database.find(id);
+    if(town == database.end())
+        return NO_VALUE;
+
+    // Starts the recursive loop.
+    auto tax = recursive_total_tax(town->second);
+
+    // Checks if the town has a parent.
+    if(town->second->parent != nullptr)
+
+        // If so, subtracts their own tax.
+        tax -= std::floor(0.1*tax);
+
+    return tax;
+}
+
+
+
+int Datastructures::distance_from_point(const Coord &coord, const Coord& point) const
+{
+    return sqrt(pow((point.x - coord.x),2.0) + pow((point.y - coord.y),2.0));
+}
+
+std::vector<TownID> Datastructures::recursive_longest_path(Town* current_town, std::vector<TownID> current, std::vector<TownID>& longest)
+{
+
+    // If the town doesn't have any vassals, returns the vector.
+    if(current_town->neighbour.empty())
+        return current;
+
+    // If the current vector is empty, inserts the id of the current town.
+    if(current.empty())
+        current.push_back(current_town->id);
+
+
+    // Goes through the neighbours.
+    for (auto& town : current_town->neighbour) {
+
+        // First the current town id is inserted.
+        current.push_back(town->id);
+
+        // Calls the fuction again.
+        auto recursive = recursive_longest_path(town, current, longest);
+
+        // If the new size is bigger than current longest, inserts the current one as longest.
+        if(recursive.size() > longest.size())
+            longest = current;
+
+        // Pops back the last element to prevent duplicates.
+        current.pop_back();
+    }
+    return current;
+}
+
+int Datastructures::recursive_total_tax(Town *current_town)
+{
+    int tax = 0;
+
+    // Checking does the town have neighbours.
+    if(current_town->neighbour.empty()) {
+        return current_town->tax;
+    }
+
+    // Loops through towns in neighbours
+    for(auto& town : current_town->neighbour) {
+
+        // Calls the function again and calculates the tax.
+        // std::floor is used to round the numbers.
+        auto recursive = recursive_total_tax(town);
+        tax += std::floor(0.1 * recursive);
+    }
+
+    // Returns the tax + current town tax.
+    return tax + current_town->tax;
 }
--- prg1/datastructures.hh
+++ prg1/datastructures.hh
@@ -97,7 +97,8 @@ public:
 
     // Estimate of performance: O(1)
     // Short rationale for estimate:
-    // Gets the amount of towns from the hash.
+    // Gets the amount of towns from the hash. Size() is constant so performance
+    // is O(1),
     unsigned int town_count();
 
     // Estimate of performance: O(N)
@@ -106,96 +107,140 @@ public:
     // in the unordered_map.
     void clear_all();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: The complexity for find-algorithm in unordered_map
+    // is O(N) in the worst case, O(1) in average cases.
+    // and the complexity for insert is O(N) in the worst case and O(1) on average.
     bool add_town(TownID id, Name const& name, Coord coord, int tax);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: The complexity for find-algorithm in unordered_map
+    // is O(N) in the worst case, O(1) in average cases.
+    // The best case is where the first element is the searched id. In this case
+    // the performance is constant.
     Name get_town_name(TownID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: The complexity for find-algorithm in unordered_map
+    // is O(N) in the worst case, O(1) in average cases.
+    // The best case is where the first element is the searched id. In this case
+    // the performance is constant.
     Coord get_town_coordinates(TownID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: TThe complexity for find-algorithm in unordered_map
+    // is O(N) in the worst case, O(1) in average cases.
+    // The best case is where the first element is the searched id. In this case
+    // the performance is constant.
     int get_town_tax(TownID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: For-loop is looping through the whole
+    // database so it is O(N).
     std::vector<TownID> all_towns();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: For-loop is looping through the whole
+    // database so it is O(N).
     std::vector<TownID> find_towns(Name const& name);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: The complexity for find-algorithm in unordered_map
+    // is O(N) in the worst case, O(1) in average cases.´
     bool change_town_name(TownID id, Name const& newname);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(NlogN)
+    // Short rationale for estimate: For-loop loops through the database to store
+    // Town-poninters to a vector => O(N), std::sort has complexity of NlogN
+    // this means that the whole function is O(N). In this function std::sort is
+    // faster than inserting to a map which is used in the next function.
     std::vector<TownID> towns_alphabetically();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(NlogN)
+    // Short rationale for estimate: For-loop loops through the database to store
+    // Town-pointers to a vector => O(N). In this function map inserting is much
+    // more faster. This was decided by running a lot of tests with std::sort and
+    // map inserting. Map sorts the elements when inserted.
     std::vector<TownID> towns_distance_increasing();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: Min_element-algorithm has complexity of O(N)
+    // except when the vector is empty.
     TownID min_distance();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: Max_element-algorithm has complexity of O(N)
+    // except when the vector is empty.
     TownID max_distance();
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: The complexity for find-algorithm in unordered_map
+    // is O(N) in the worst case, O(1) in average cases.
+    // due to rounding it doesn't matter if there is multiple find-algorithms in
+    // row. Push_back has complexity of O(1) so the function is O(N)
     bool add_vassalship(TownID vassalid, TownID masterid);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: Find-algorithm has a complexity of O(N)
+    // and for-loop also is O(N), so the function is O(N).
     std::vector<TownID> get_town_vassals(TownID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: The complexity for find-algorithm in unordered_map
+    // is O(N) in the worst case, O(1) in average cases
+    // and while loop loops until it will find a element, whose parent is nullptr
+    // so in the worst program has to go through the whole database.
     std::vector<TownID> taxer_path(TownID id);
 
     // Non-compulsory phase 1 operations
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: This function will first check if the element'
+    // is in the database which is O(N), after this the function will either have
+    // a for-loop or another for-loop and a clear function. All of these are O(N)
+    // so the whole function is O(N). The best case is O(1) where all of the elements
+    // are found in the beginning of vectors.
     bool remove_town(TownID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(NlogN)
+    // Short rationale for estimate: This function will first go through the database
+    // and insert the distances and IDs to a map and the map will sort the IDs
+    // according to distance. After this The function will go through another
+    // loop, but it doesn't change the performance.
     std::vector<TownID> towns_nearest(Coord coord);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: The worst case is O(N) since there is a find
+    // function, then the function will go through the whole database which is
+    // linear => O(N).
     std::vector<TownID> longest_vassal_path(TownID id);
 
-    // Estimate of performance:
-    // Short rationale for estimate:
+    // Estimate of performance: O(N)
+    // Short rationale for estimate: In the worst case the function will have to
+    // go through the whole database, which is linear. The function also uses find function which
+    // is O(N). The best case is constant O(1) where the wanted town is first
+    // in the database and their neighbours are nullptr.
     int total_net_tax(TownID id);
 
 private:
+
+
     struct Town
     {
-        Name name;
-        Coord coord;
-        int tax;
+        TownID id = NO_TOWNID;
+        Name name = NO_NAME;
+        Coord coord = NO_COORD;
+        int tax = NO_VALUE;
 
-        Town* vessel;
+        Town* parent = nullptr;
+        std::vector<Town*> neighbour = {};
     };
 
+    int distance_from_point(const Coord& coord, const Coord& point) const;
+    std::vector<TownID> recursive_longest_path(Town* current_town, std::vector<TownID> current, std::vector<TownID>& longest);
+    int recursive_total_tax(Town* current_town);
 
     std::unordered_map<TownID, Town*> database;
-
-
-
-
 
 
 };
